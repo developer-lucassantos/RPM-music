8d4b6e8eda9e5555d063859067b6d1e175024bb0
lucasph-dev <lsantoss.dev@gmail.com>
2026-03-16 17:31:11 -0300

    feat: adiciona novos artistas, botões flutuantes, página de planos e otimização mobile

--- js/script.js
+++ js/script.js
@@ -72,3 +72,16 @@ navLinks.forEach((l) => {
         }
     });
 });
+
+window.onscroll = function() {
+    const btn = document.getElementById("backToTop");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+document.getElementById("backToTop").onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
